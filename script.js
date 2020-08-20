47d121e8ff9f82df735604edfeb1989230008168
Larraine Gonzales <larrainegonzales@hotmail.com>
2020-08-20 13:43:22 -0500

    updated javascript and html

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function searchForCityByName() {
       fields: ["name", "geometry"]
     };
     service = new google.maps.places.PlacesService(map);
-    service.findPlaceFromQuery(request, function(results, status) {
+    service.findPlaceFromQueryOne(request, function(results, status) {
       if (status === google.maps.places.PlacesServiceStatus.OK) {
         for (var i = 0; i < results.length; i++) {
           createMarker(results[i]);
